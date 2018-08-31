24973442c557fece1ad9bf8e5b851a5932b652dd
Gordon <gordon.matthew@gmail.com>
2018-08-31 21:35:25 +0800

    added server tests

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,4 +3,4 @@ var mongoose =  require('mongoose');
 mongoose.Promise =  global.Promise;
 mongoose.connect('mongodb://localhost:27017/Todoapp');
 
-module.export = {mongoose};
+module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -25,4 +25,4 @@ app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
-//module.exports = {app};
+module.exports = {app};
